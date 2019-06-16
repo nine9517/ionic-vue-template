944cdeda7c9b87eae44fdffbff213d245c967486
TheNineInwKung <nine4567@gmail.com>
2019-06-16 14:42:12 +0700

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import VueRouter from 'vue-router';
+import '@ionic/core/css/core.css';
+import '@ionic/core/css/ionic.bundle.css';
+import routes from './routes';
 
-Vue.config.productionTip = false
+import IonicVue from '@ionic/vue';
+
+Vue.config.productionTip = false;
+
+Vue.use(IonicVue);
+Vue.use(VueRouter);
+
+const router = new VueRouter({routes});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    router,
+    render: h => h(App)
+}).$mount('#app');
